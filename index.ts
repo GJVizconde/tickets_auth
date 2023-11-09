f43c54a629135d096ae920fb40efe2663eed333f
Gilmer J. Vizconde <gilmerjose.vizconde2@gmail.com>
2023-11-09 14:18:53 -0500

    add Todos

--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ import { PORT } from './src/config/env.config'
 
 const port = PORT || 3001
 
+//TODO: Desplegar en AWS
+//TODO: Probar manejar entorno Production and development with Prisma
+
 app.listen(port, () => {
   console.log('Listening on port %s', port)
 })
